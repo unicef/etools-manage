f8987594e71644161d1140c9dcd331e9f5730e9d
Marko Bilal <marko.barca10@gmail.com>
2019-03-29 17:42:16 -0400

    yay

--- src/lib/create-saga.ts
+++ src/lib/create-saga.ts
@@ -3,17 +3,18 @@ import { cloneDeep } from 'micro-dash';
 import { all, fork, take, CallEffectFn } from 'redux-saga/effects';
 import { onRouteTransition } from '../actions';
 
-export function* createSaga(sagas: CallEffectFn<any>, args?) {
-    const runningSagas = yield all(arrify(sagas).map(saga => fork(saga, args)));
+export function* createSaga(sagas: CallEffectFn<any>, ...args) {
+    // @ts-ignore
+    const runningSagas = yield all(arrify(sagas).map(saga => fork(saga, ...args)));
     yield take(onRouteTransition.toString());
     runningSagas.forEach(saga => saga.cancel());
 }
 
-export function* createAsyncPageSaga(sagas, args?) {
+export function* createAsyncPageSaga(sagas, ...args) {
     // Must be while(true) since these are only injected once on initial page load.
     // When navigating away and back to a page, we need to recreate the sagas for
     // proper initialization.
     while (true) {
-        yield createSaga(sagas, cloneDeep(args));
+        yield createSaga(sagas, ...cloneDeep(args));
     }
 }
