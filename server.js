53898a6db4587ee41c0695fd4d8006c07f0ff6ef
Marko <marko.barca10@gmail.com>
2019-08-22 16:06:53 -0400

    fix public path

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const app = express();
 const port = 8080
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', function(req, res) {
+  console.log(req.url);
+  console.log(req.headers);
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Etools manage listening on port ${port}!`));
+app.listen(port, () => console.log(`Etools manage listening on port ${port}!`)); 
